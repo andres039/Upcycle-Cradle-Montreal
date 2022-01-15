3d981e8539de30aedb5955a50355f21278683dc7
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-01-15 19:24:51 +0000

    Add ability to add a pin where the mouse is clicked

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,9 +1,26 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useState } from 'react'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 
 const Map = () => {
   const mapPosition = [45.4, -75.7];
   const mapTilesId = 'mapbox/streets-v11';
   const mapAccessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+  function NewMarker() {
+    const [pinPosition, setPinPosition] = useState(null)
+    
+    useMapEvents({
+      click(e) {
+        setPinPosition(e.latlng)
+      }
+    })
+  
+    return pinPosition === null ? null : (
+      <Marker position={pinPosition}>
+        <Popup>You are here</Popup>
+      </Marker>
+    )
+  }
 
   return(
     <MapContainer center={mapPosition} zoom={13} style={{ width: '75vw', height: '100vh'}}>
@@ -11,14 +28,10 @@ const Map = () => {
         attribution='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
         url={`https://api.mapbox.com/styles/v1/${mapTilesId}/tiles/{z}/{x}/{y}?access_token=${mapAccessToken}`}
       />
+      <NewMarker />
       <Marker position={mapPosition}>
         <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-      <Marker position={[45.4, -75.1]}>
-        <Popup>
-          Another popup!
+          A pretty CSS3 popup. <br /> Easily customizable.<button>Delete</button>
         </Popup>
       </Marker>
     </MapContainer>
