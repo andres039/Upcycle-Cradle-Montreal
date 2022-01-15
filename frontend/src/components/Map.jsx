c6ec4a76ca8a23439e643790362b17d479ddf0ed
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-01-15 23:31:21 +0000

    Add an object to contain data about the new pin including the coordinates

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -6,24 +6,34 @@ const Map = () => {
   const mapTilesId = 'mapbox/streets-v11';
   const mapAccessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-  // array of locations for testing
-  const locations = [
+  // array of saved items for testing
+  const savedItems = [
     {
+      id: 1,
       title: "Location 1",
       description: "Description of location 1.",
       coordinates: [45.4310, -75.7372]
     },
     {
+      id: 2,
       title: "Location 2",
       description: "Description of location 2.",
       coordinates: [45.4411, -75.7592]
     },
     {
+      id: 3,
       title: "Location 3",
       description: "Description of location 3.",
       coordinates: [45.4121, -75.7612]
     }
   ];
+
+  // object of new item for testing
+  const newItem = {
+    id: 4,
+    title: "Location 4",
+    description: "Description of location 4."
+  };
 
   const NewMarker = () => {
     const [pinPosition, setPinPosition] = useState(null);
@@ -31,13 +41,16 @@ const Map = () => {
     useMapEvents({
       click(e) {
         setPinPosition(e.latlng);
+        newItem.coordinates = [e.latlng.lat, e.latlng.lng];
+        console.log(newItem);
       }
     });
   
     return pinPosition === null ? null : (
       <Marker position={pinPosition}>
         <Popup>
-          You are here
+          <h1>{newItem.title}</h1>
+          <p>{newItem.description}</p>
           <button onClick={() => setPinPosition(null)}>Delete</button>
         </Popup>
       </Marker>
@@ -51,7 +64,7 @@ const Map = () => {
         url={`https://api.mapbox.com/styles/v1/${mapTilesId}/tiles/{z}/{x}/{y}?access_token=${mapAccessToken}`}
       />
       <NewMarker />
-      { locations.map(item => (
+      { savedItems.map(item => (
         <Marker position={item.coordinates}>
           <Popup>
             <h1>{item.title}</h1>
