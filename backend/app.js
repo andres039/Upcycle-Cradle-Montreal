24d854524459b188b0890984d7aa07a0afef3cce
slipova <sofiya.lipova@mail.mcgill.ca>
2022-01-16 18:14:47 -0500

    fix backend routes

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/api/")
 
+/////////////////////////
+
+// app.use("/api/pins", pins(db));
+// app.use("/api/users", users(db));
+
+
+
+
+
+app.get("/api/users", (req, res) => {
+  const users = [{ name: "Jeremy" }, { name: "Furious" }];
+  res.json(users);
+})
+
+app.get("/api/users/:user_id", (req, res) => {
+  const user = { name: "Furious" };
+  res.json(user);
+})
+
+
+app.get("/api/pins", (req, res) => {
+  const pins = [{ 3: 3 }, { 4: 4 }];
+  res.json(pins);
+})
+
+app.get("/api/pins/:pin_id", (req, res) => {
+  const pin = { 3: 3 };
+  res.json(pin);
+})
 
 //fetching information from the database?
 
