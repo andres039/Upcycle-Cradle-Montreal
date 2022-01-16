0fefff46e76c53a122b4fd91a712a188786e575f
slipova <sofiya.lipova@mail.mcgill.ca>
2022-01-16 18:16:45 -0500

    connect api routes to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
 import Map from './components/Map';
 import './App.css';
-// import Button from './components/Button';
+import Button from './components/Button';
 import LoginForm from './components/LoginForm';
 import NewItemForm from './components/SideBar/NewItemForm';
 import RegistrationForm from './components/RegistrationForm';
@@ -8,10 +12,22 @@ import RegistrationForm from './components/RegistrationForm';
 // import LoginForm from './components/LoginForm';
 
 function App() {
+
+  const [users, setUsers] = useState()
+  const [pins, setPins] = useState()
+
+  useEffect(() => {
+    axios.get("/api/users").then((res) => setUsers(res.data));
+  }, []);
+
+  useEffect(() => {
+    axios.get("/api/pins").then((res) => setPins(res.data));
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Map />
+      {/* <Map /> */}
     </div>
   );
 }
--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -5,8 +5,8 @@ const Map = () => {
   const mapTilesId = 'mapbox/streets-v11';
   const mapAccessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-  return(
-    <MapContainer center={mapPosition} zoom={13} style={{ width: '75vw', height: '100vh'}}>
+  return (
+    <MapContainer center={mapPosition} zoom={13} style={{ width: '75vw', height: '100vh' }}>
       <TileLayer
         attribution='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
         url={`https://api.mapbox.com/styles/v1/${mapTilesId}/tiles/{z}/{x}/{y}?access_token=${mapAccessToken}`}
