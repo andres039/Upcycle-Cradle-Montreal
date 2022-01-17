76aa4cc0c2a3fc6d4b39f091c12b47edad73f99f
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-01-17 19:25:39 +0000

    Add component to center map at user location

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,47 +1,44 @@
-import { useEffect, useRef } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { useEffect } from 'react';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 
 import Pin from './Pin';
 
+// Set map to center at user location
+const UserLocation = () => {
+  const map = useMap();
+  useEffect(() => {
+    map.locate({setView: true});
+  }, []);
+  return null;
+}
+
 const Map = () => {
-  const mapPosition = [45.4, -75.7];
+  const mapPosition = [45.4, -73.6];
   const mapTilesId = 'mapbox/streets-v11';
   const mapAccessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-  // Set map to geolocation but not currently working
-  // const mapRef = useRef();
-
-  // useEffect(() => {
-  //   const { current = {} } = mapRef;
-  //   const { leafletElement: map } = current;
-    
-  //   map.locate({
-  //     setView: true
-  //   });
-  // }, []);
-  
   // array of saved items for testing
   const savedItems = [
     {
       id: 1,
       title: "Item 1",
       description: "Description of Item 1.",
-      latitude: 45.4310,
-      longitude: -75.7372
+      latitude: 45.432,
+      longitude: -73.623
     },
     {
       id: 2,
       title: "Item 2",
       description: "Description of Item 2.",
-      latitude: 45.4411,
-      longitude: -75.7592
+      latitude: 45.507,
+      longitude: -73.690
     },
     {
       id: 3,
       title: "Item 3",
       description: "Description of Item 3.",
-      latitude: 45.4121,
-      longitude: -75.7612
+      latitude: 45.527,
+      longitude: -73.583
     }
   ];
 
@@ -59,11 +56,12 @@ const Map = () => {
   });
 
   return(
-    <MapContainer center={mapPosition} zoom={13} style={{ width: '75vw', height: '100vh'}}>
+    <MapContainer center={mapPosition} style={{ width: '75vw', height: '100vh'}}>
       <TileLayer
         attribution='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
         url={`https://api.mapbox.com/styles/v1/${mapTilesId}/tiles/{z}/{x}/{y}?access_token=${mapAccessToken}`}
       />
+      <UserLocation/>
       <Pin item={newItem} allItems={savedItems}/>
       {parsedPins}
     </MapContainer>
