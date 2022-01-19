cd61f8017cb97b36a9b14a2de101c23a44d98707
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-01-19 03:47:36 +0000

    Add latitude and longitude state to Mapview component

--- frontend/src/pages/MapView.jsx
+++ frontend/src/pages/MapView.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import { Link } from "react-router-dom";
 import Map from "../components/Map";
 import SidebarHeader from "../components/SidebarHeader";
 import Instructions from "../components/Instructions";
 
 const MapView = () => {
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
   return (
     <div className="container">
 
-      <Map />
+      <Map
+        latitude={latitude}
+        longitude={longitude}
+        setLatitude={setLatitude}
+        setLongitude={setLongitude}
+      />
       <section className="sidebar">
 
         <SidebarHeader />
