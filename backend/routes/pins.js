f28838c846d9e9b087f841ab53255af104aaba94
Andres <andres039039@hotmail.com>
2022-01-23 14:41:42 -0500

    sets up new pin

--- backend/routes/pins.js
+++ backend/routes/pins.js
@@ -2,15 +2,14 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
 const { Pool } = require("pg");
+const { database } = require("pg/lib/defaults");
 const dbParams = require("../lib/db.js");
 const db = new Pool(dbParams);
-const database = require("./dbQueries/pinQueries");
 db.connect();
 
 //Select all pins
 router.get("/api/pins", (req, res) => {
-  database
-    .getAllPins(db)
+  db.query("SELECT * FROM pins;")
     .then((response) => res.send(response.rows))
     .catch((err) => {
       console.log("API/pins error:", err);
@@ -21,8 +20,7 @@ router.get("/api/pins", (req, res) => {
 //Select individual pins
 
 router.get("/api/pins/:id", (req, res) => {
-  database
-    .getPinsById(db, req.params.id)
+  db.query("SELECT * FROM pins WHERE id=$1;", [req.params.id])
     .then((response) => res.send(response.rows))
     .catch((err) => {
       console.log("API/pins error:", err);
@@ -33,8 +31,7 @@ router.get("/api/pins/:id", (req, res) => {
 //Delete individual pins
 
 router.delete("/api/pins/:id", (req, res) => {
-  database
-    .deletePins(db, req.params.id)
+  db.query("DELETE FROM pins WHERE id = $1 RETURNING *;", [req.params.id])
     .then((response) => {
       res.send(200);
     })
@@ -47,13 +44,15 @@ router.delete("/api/pins/:id", (req, res) => {
 //Update individual pins
 
 router.put("/api/pins/:id", async (req, res) => {
-  console.log("PUT/API/PINS", req.body);
   res.status(200).send();
 
   try {
     const { current_user_id, pinID } = req.body;
-    await database
-      .updateIndividualPins(db, current_user_id, pinID)
+    const updatedPin = await db
+      .query("UPDATE pins SET claimer_id = $1 WHERE id = $2 RETURNING *;", [
+        current_user_id,
+        pinID,
+      ])
       .then((response) => {
         res.json(response.rows);
       });
@@ -66,6 +65,7 @@ router.put("/api/pins/:id", async (req, res) => {
 
 router.post("/api/pins", async (req, res) => {
   try {
+    //test compare with users
     const verification = jwt.verify(req.headers.token, process.env.TOKEN_KEY);
     const {
       title,
@@ -79,19 +79,17 @@ router.post("/api/pins", async (req, res) => {
       claimer_id,
     } = req.body;
 
-    const newPin = database.insertNewPin(
-        db,
-        title,
-        description,
-        picture,
-        condition,
-        latitude,
-        longitude,
-        date,
-        creator_id,
-        claimer_id,
-    )
-      
+    const newPin = await database.insertNewPin(
+      db,
+      title,
+      description,
+      picture,
+      condition,
+      latitude,
+      longitude,
+      date,
+      creator_id,
+      claimer_id
     );
     res.json(newPin);
   } catch (err) {
