1f2498c775ea6b0d1f62a2bc04cb41d34252942d
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-01-25 19:47:08 +0000

    Add about us text to the welcome page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,13 +9,19 @@ const Home = () => {
       <div className="welcome-page__text">
         <img className="login-page__logo" src="images/logo.png" alt="Trash Panda Montreal" />
       
-        <Link to="/login">
-          <span className="welcome-page__login-link">Log in</span>
-        </Link>
-        <Link to="/register">
-          <span className="welcome-page__register-link">Register</span>
-        </Link>
+        <div>
+          <h1>Welcome</h1>
+          <p className="welcome-page__about">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis et esse distinctio facere, minima, asperiores non suscipit officiis excepturi dolorem magnam repellat, similique velit. Maiores, laudantium. Sint ad eius minus!</p>
+        </div>
       </div>
+      <div className="welcome-page__links">
+        <Link to="/login">
+            <span className="welcome-page__login-link">Log in</span>
+          </Link>
+          <Link to="/register">
+            <span className="welcome-page__register-link">Register</span>
+          </Link>
+        </div>
     </main>
   );
 };
